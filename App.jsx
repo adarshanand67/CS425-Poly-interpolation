9e52af16de60dd3dfba89165e914f2b29c049ff3
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-04-12 23:18:21 +0530

    .

--- App.jsx
+++ App.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import Plot from "react-plotly.js";
 import PolynomialGraph from "./PolynomialGraph";
-// 12. Polynomial Interpolator:Write a GUI based tool that reads a prime number q and hence n + 1
-// evaluations of a univariate polynomial f(x) over Fq as input and demonstrates the sequence of steps
-// involved in the reconstruction of f, assuming f to be of degree n.
 
 const App = () => {
   const [q, setQ] = useState(0);
@@ -12,13 +9,10 @@ const App = () => {
   const [evaluationsY, setEvaluationsY] = useState([]);
   const [interpolated, setInterpolated] = useState(false);
   const [coefficients, setCoefficients] = useState([]);
-  // const evaluations = zip(evaluationsX, evaluationsY); // zip the x and y coordinates
-  // console.log(evaluations)
+
   console.log(evaluationsX, evaluationsY);
 
-  // include field size q in all calculations
   function polyEval(coeffs, x, q) {
-    // given coeffs[0] + coeffs[1] x + ... + coeffs[n] x^n, return the value of the polynomial at x
     var y = 0;
     var i;
     for (i = coeffs.length; i-- > 0; ) y = y * x + coeffs[i];
@@ -27,7 +21,6 @@ const App = () => {
   }
 
   function polyAdd(aCoeffs, bCoeffs, q) {
-    //sum of two polynomials, given as arrays of coefficients
     var ret = [];
     var i;
 
@@ -40,7 +33,6 @@ const App = () => {
   }
 
   function polyMul(aCoeffs, bCoeffs, q) {
-    // product of two polynomials, given as arrays of coefficients
     var ret = [];
     var i, j;
 
@@ -56,7 +48,7 @@ const App = () => {
   function lagrangeInterpolate(points, q) {
     console.log(points, q);
     q = parseInt(q);
-    // given an array of points, return the coefficients of the Lagrange interpolating polynomial
+
     var ret = [0];
     var i, j;
 
@@ -79,16 +71,11 @@ const App = () => {
     return ret;
   }
 
-  // const q=5
-  // const points = [{x: 0, y: 1}, {x: 1, y: 0},{x: 2, y: 1}]
-  // console.log(lagrangeInterpolate(points))
   const interpolate = (e, q, n, evaluationsX, evaluationsY) => {
     e.preventDefault();
     setInterpolated(true);
     const zip = (a, b) => a.map((k, i) => [k, b[i]]);
-    // const a = [1,2,3,4,5,6,7,8,9,10]
-    // const b = [1,2,3,4,5,6,7,8,9,10]
-    // const evaluations = zip(a, b); // zip the x and y coordinates
+
     console.log(evaluationsX, evaluationsY);
     const a = [];
     for (let i = 0; i < evaluationsX.length; i++) {
@@ -118,38 +105,33 @@ const App = () => {
         <b>Input:</b> n+1 points (x<sub>i</sub>, y<sub>i</sub>) on a curve over
         F<sub>q</sub>, where q is a prime number
       </p>
-        <p>
-          <b>Note:-</b>
-          Input comma separated values for x and y coordinates (no space) <br />
-          <b>Example</b> - Input q=5,n=3,x=0,1,2,y=1,0,1
-
-        </p>
+      <p>
+        <b>Note:-</b>
+        Input comma separated values for x and y coordinates (no space) <br />
+        <b>Example</b> - Input q=5,n=3,x=0,1,2,y=1,0,1
+      </p>
 
       {/* input field q */}
       <input
         type="number"
-        // value={q}
         placeholder="Enter q"
         onChange={(e) => setQ(e.target.value)}
       />
       {/* input field n */}
       <input
         type="number"
-        // value={n}
         placeholder="Enter n"
         onChange={(e) => setN(e.target.value)}
       />
       {/* n+1 space separated numbers x-coordinate */}
       <input
         type="text"
-        // value={evaluationsX}
         placeholder="Enter evaluations for x"
         onChange={(e) => setEvaluationsX(e.target.value.split(","))}
       />
       {/* n+1 space separated numbers y-coordinate */}
       <input
         type="text"
-        // value={evaluationsY}
         placeholder="Enter evaluations for y"
         onChange={(e) => setEvaluationsY(e.target.value.split(","))}
       />
@@ -190,7 +172,7 @@ const App = () => {
             {evaluationsX.map((x, i) => (
               <span key={i}>
                 f({x})={evaluationsY[i]}{" "}
-                {coefficients.reduce((acc, c, i) => { // evaluate the polynomial at x
+                {coefficients.reduce((acc, c, i) => {
                   acc += c * Math.pow(x, i);
                   return acc;
                 }) %
@@ -202,10 +184,6 @@ const App = () => {
               </span>
             ))}
           </p>
-          {/* Graph */}
-          {/* <h2>Graph</h2> */}
-          {/* <PolynomialGraph coefficients={coefficients} /> */}
-          {/* <img src="./poly.png"/> */}
         </div>
       )}
     </>
